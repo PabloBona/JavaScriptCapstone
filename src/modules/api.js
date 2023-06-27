fb9e7b3747e1691cc5726a642a42a13ceb0f0748
PabloBona <pablobonasera@gmail.com>
2023-06-27 19:19:26 -0300

    Improve API call Add involmentApi Add like Count at renderShows Merge API calls values

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,5 +1,14 @@
 const showApiBaseUrl = 'https://api.tvmaze.com';
 
-const getShows = () => fetch(`${showApiBaseUrl}/shows`);
+const getShows = async (limit = 12) => {
+  const response = await fetch(`${showApiBaseUrl}/shows`);
+  if (!response.ok) {
+    throw new Error('Failed to fetch Series');
+  }
+
+  const shows = await response.json();
+
+  return shows.slice(0, limit);
+};
 
 export default getShows;
