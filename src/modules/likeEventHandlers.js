92b170e5be453db1b301d816267100f87f8b34bf
PabloBona <pablobonasera@gmail.com>
2023-06-28 17:15:09 -0300

    Improve add-likes functionality

--- src/modules/likeEventHandlers.js
+++ src/modules/likeEventHandlers.js
@@ -8,12 +8,14 @@ export default () => {
       const countElem = e.target.querySelector('.like-count');
       const likeCount = parseInt(countElem.dataset.likes, 10);
       const errorElem = document.querySelector('.error-message');
-      try {
-        await addLike(parseInt(showId, 10));
-        likeBtn.classList.add('liked');
-        countElem.textContent = likeCount + 1;
-      } catch (error) {
-        errorElem.textContent = 'Failed to add like';
+      if (!e.target.classList.contains('liked')) {
+        try {
+          await addLike(parseInt(showId, 10));
+          likeBtn.classList.add('liked');
+          countElem.textContent = likeCount + 1;
+        } catch (error) {
+          errorElem.textContent = 'Failed to add like';
+        }
       }
     });
   });
