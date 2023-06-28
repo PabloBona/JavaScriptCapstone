996696ae093b4e8979142bcc5fc2b5d1b4316705
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-28 10:51:53 -0400

    Add the function renderCommentsList in to display the comments section in the modal

--- src/modules/handlerEvents.js
+++ src/modules/handlerEvents.js
@@ -1,12 +1,15 @@
 import { getShowDetails } from './api.js';
-import { renderShowDetails } from './renderCommentsModal.js';
+import { renderShowDetails, renderCommentsList } from './renderCommentsModal.js';
+import { getShowComments } from './involvementApi.js';
 
 const showHideModalEventHandlers = () => {
   const commentsModal = document.getElementById('showCommentsPopup');
   commentsModal.addEventListener('show.bs.modal', async (e) => {
     const showId = Number(e.relatedTarget.dataset.index);
     const showDetails = await getShowDetails(showId);
+    const showCommentsList = await getShowComments(showId);
     renderShowDetails(showDetails);
+    renderCommentsList(showCommentsList);
   });
 
   commentsModal.addEventListener('hide.bs.modal', async () => {
