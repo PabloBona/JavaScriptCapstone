ceac7166113f4026c8890ec2bd173c7a91bb8602
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-28 00:46:21 -0400

    Create functions to render the comments modal in the DOM

---
+++ src/modules/renderCommentsModal.js
@@ -0,0 +1,58 @@
+export const renderBaseTemplateModal = () => {
+  const footerElement = document.querySelector('footer');
+  footerElement.insertAdjacentHTML(
+    "afterend",
+    `<div class="modal fade" id="showCommentsPopup" 
+          data-bs-backdrop="static" data-bs-keyboard="false" 
+          tabindex="-1" aria-labelledby="showCommentsPopupLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div id="container-show-details"></div>
+            <div id="container-list-comments"></div>
+            <div id="container-add-comments"></div>
+          </div>
+        </div>
+      </div>
+    </div>`
+  );
+};
+
+export const renderShowDetails = ({ name, genres, image, language, premiered, rating, _embedded }) => {
+  const seasons = _embedded.seasons.length;
+  const premieredFormatted = new Date(premiered).toLocaleDateString('en-us', { year:'numeric', month:'long', day: 'numeric'});
+
+  const showDetailsContainer = document.getElementById('container-show-details');
+  showDetailsContainer.innerHTML = `
+  <div class="row justify-content-center align-items-center">
+    <div class="col-12 d-flex justify-content-center align-items-center">
+      <img src="${image.original}" alt="" id="showImage" class="img-fluid" /> 
+    </div>
+  </div>
+  <h2 class="modal-title fs-1-extra py-4 text-center fw-bolder">${name}</h2>
+  <div class="row justify-content-center">
+    <div class="col-10">
+      <div class="row">
+        <div class="col-md-6">
+          <p class="fs-5"><b>Language: </b> <span>${language}</span></p>
+        </div>
+        <div class="col-md-6">
+          <p class="fs-5"><b>Premiered: </b> <span>${premieredFormatted}</span></p>
+        </div>
+        <div class="col-md-6">
+          <p class="fs-5"><b>Rating: </b> <span>${rating.average}</span></p>
+        </div>
+        <div class="col-md-6">
+          <p class="fs-5"><b>Seasons: </b> <span>${seasons} season${seasons !== 1 ? 's': ''}</span></p>
+        </div>
+        <div class="col-md-12">
+          <p class="fs-5"><b>Genres: </b> <span>${genres.join(' | ')}</span></p>
+        </div>
+      </div>
+    </div>
+  </div>
+  `;
+};
