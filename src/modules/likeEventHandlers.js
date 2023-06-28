9d5017d2ea839bb22aa8917ca831b57b7f7b4039
PabloBona <pablobonasera@gmail.com>
2023-06-28 17:32:49 -0300

    Add a isRequestInProgres boolean to avoid multiples POST to API

--- src/modules/likeEventHandlers.js
+++ src/modules/likeEventHandlers.js
@@ -2,19 +2,26 @@ import { addLike } from './involvementApi.js';
 
 export default () => {
   const likeBtns = document.querySelectorAll('.btn-like');
+
+  let isRequestInProgress = false; // Variable para controlar si hay una solicitud en curso
+
   likeBtns.forEach((likeBtn) => {
     likeBtn.addEventListener('click', async (e) => {
       const { showId } = e.target.dataset;
       const countElem = e.target.querySelector('.like-count');
       const likeCount = parseInt(countElem.dataset.likes, 10);
       const errorElem = document.querySelector('.error-message');
-      if (!e.target.classList.contains('liked')) {
+
+      if (!isRequestInProgress && !e.target.classList.contains('liked')) {
         try {
+          isRequestInProgress = true;
           await addLike(parseInt(showId, 10));
           likeBtn.classList.add('liked');
           countElem.textContent = likeCount + 1;
         } catch (error) {
           errorElem.textContent = 'Failed to add like';
+        } finally {
+          isRequestInProgress = false;
         }
       }
     });
