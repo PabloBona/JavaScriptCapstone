388c382a3be255b88b3bdc064bffae4958a94e2c
Nitcelis Bravo <nbravoalcala@gmail.com>
2023-06-28 23:21:59 -0400

    Add a submit event listener for the Add a Comment form

--- src/modules/handlerEvents.js
+++ src/modules/handlerEvents.js
@@ -1,6 +1,36 @@
 import { getShowDetails } from './api.js';
-import { renderShowDetails, renderCommentsList } from './renderCommentsModal.js';
-import { getShowComments } from './involvementApi.js';
+import { renderShowDetails, renderCommentsList, renderCommentsForm, commentItemMarkup } from './renderCommentsModal.js';
+import { getShowComments, saveNewComment } from './involvementApi.js';
+
+const AddCommentEventHandler = () => {
+  const formAddComment = document.getElementById('add-comment-form');
+  formAddComment.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    
+    const { inputUsername, inputComment } = formAddComment;
+    const showId = Number(formAddComment.dataset.show);
+    
+    const dataComment = {
+      item_id: showId,
+      username: inputUsername.value,
+      comment: inputComment.value
+    };
+    
+    formAddComment.reset();
+
+    const saveCommentResult = await saveNewComment(dataComment);
+    if(saveCommentResult) {
+      const showCommentsList = await getShowComments(showId);
+      renderCommentsList(showCommentsList);
+    } else {
+      const errorContainer = document.getElementById('add-comment-error');
+      errorContainer.textContent = 'Error! Comment not saved. Refresh the page and Try again.';
+      setTimeout(() => {
+        errorContainer.textContent = '';
+      }, 4500);
+    }
+  });
+}
 
 const showHideModalEventHandlers = () => {
   const commentsModal = document.getElementById('showCommentsPopup');
@@ -10,11 +40,14 @@ const showHideModalEventHandlers = () => {
     const showCommentsList = await getShowComments(showId);
     renderShowDetails(showDetails);
     renderCommentsList(showCommentsList);
+    renderCommentsForm(showId);
+    AddCommentEventHandler();
   });
 
   commentsModal.addEventListener('hide.bs.modal', async () => {
     document.getElementById('container-show-details').innerHTML = '';
     document.getElementById('container-list-comments').innerHTML = '';
+    document.getElementById('container-add-comments').innerHTML = '';
   });
 };
 
